0e505348cdbd7f289791411b3e5524d6b6a2c33a
dickzhang <343178362@qq.com>
2019-11-28 00:29:55 +0800

    typehead, modal

--- src/angular/src/app/app.module.ts
+++ src/angular/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +9,8 @@ import { Demo1Component } from './demo1/demo1.component';
 import { Demo2Component } from './demo2/demo2.component';
 import { Demo3Component } from './demo3/demo3.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ChildNodeComponent } from './child-node/child-node.component';
 
 @NgModule({
   declarations: [
@@ -18,18 +18,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     Demo1Component,
     Demo2Component,
     Demo3Component,
-    HomeComponent
+    HomeComponent,
+    ChildNodeComponent
   ],
   imports: [
     NgbModule,
-    BrowserModule,
-    HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
+    BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ChildNodeComponent]
 })
 export class AppModule { }
---
+++ src/angular/src/app/child-node/child-node.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-node',
+  templateUrl: './child-node.component.html',
+  styleUrls: ['./child-node.component.scss']
+})
+export class ChildNodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
